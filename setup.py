8c2ef9e09c0ed465345d9e3c709c883dd68f0a54
louiisliu <11610522@mail.sustech.edu.cn>
2020-07-22 17:05:07 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+import os
+
+setuptools.setup(
+    name="mizore",
+    version="0.1.0",
+    description="A core library of the scientific code for Orquestra.",
+    packages=setuptools.find_namespace_packages(),
+    # install_requires=[
+    #     'numpy>=1.18.1',
+    # ]
+)
