8153d946e1875d7a3ecb66d6b573e1bca97a6417
louiisliu <11610522@mail.sustech.edu.cn>
2020-07-22 17:05:28 +0800

    Add dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="0.1.0",
     description="A core library of the scientific code for Orquestra.",
     packages=setuptools.find_namespace_packages(),
-    # install_requires=[
-    #     'numpy>=1.18.1',
-    # ]
+    install_requires=[
+        'numpy>=1.18.1',
+    ]
 )
