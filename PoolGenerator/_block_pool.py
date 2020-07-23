fbb631eefec10eb3741cafe460349beaf8d8c323
doomspec <doomspec@outlook.com>
2020-07-23 14:22:37 +0800

    Add random selection and merge of BlockPool

--- PoolGenerator/_block_pool.py
+++ PoolGenerator/_block_pool.py
@@ -1,5 +1,4 @@
-# Mixed Pool
-# Random Pool
+import math
 
 class BlockPool:
     """Base class of block pools
@@ -23,5 +22,30 @@ class BlockPool:
         for block in self.blocks:
             info+=str(block)+"\n"
         return info
+        
     def __iadd__(self,block):
         self.blocks.add(block)
+    
+    def __add__(self,pool):
+        self.merge_with_another_pool(pool)
+
+    def generate_random_reduced_pool(self,n_block=0,percent=0):
+        if n_block<=0 and (percent>=1 or percent<=0):
+            print("Invalid parameter for random pool reduce! Self returned.")
+            return self
+        if n_block==0:
+            n_block=math.ceil(len(self.blocks)*percent)
+
+        new_block_set=random_select(self.blocks,n_block)
+        pool=BlockPool()
+        pool.blocks=new_block_set
+        return new_block_set
+    
+    def merge_with_another_pool(self,another_pool:BlockPool):
+        self.blocks=set.union(another_pool.blocks,self.blocks)
+
+
+import numpy
+def random_select(collection=set(), num=1):
+    return set(numpy.random.choice(numpy.array(list(collection),dtype=object), size=num, replace=False))
+
