8656ce375b96762368531a6451b03d4268801fe6
doomspec <doomspec@outlook.com>
2020-07-23 16:03:06 +0800

    Refined ITE

--- ParameterOptimizer/_imaginary_time_evolution_optimizer.py
+++ ParameterOptimizer/_imaginary_time_evolution_optimizer.py
@@ -1,57 +1,73 @@
 from scipy.optimize import minimize, basinhopping
 from ParameterOptimizer.ObjWrapper import get_obj_for_optimizer
 from Utilities.Tools import random_list
-#from Blocks import BlockCircuit
+from Blocks import BlockCircuit
 from ParameterOptimizer import ParameterOptimizer
 from Blocks._utilities import get_inner_two_circuit_product, get_circuit_energy
 from scipy import linalg
+import numpy
 
+NOT_DEFINED=999999
 
 class ImaginaryTimeEvolutionOptimizer(ParameterOptimizer):
 
-    def __init__(self, random_initial=0.01, diff=1e-4, stepsize=1e-1, tol=1e-6, max_acceptable_increase_n_step=10):
+    def __init__(self, random_adjust=0.01, diff=1e-4, stepsize=1e-1, n_step=10, max_increase_n_step=3, task_manager=None):
         ParameterOptimizer.__init__(self)
 
-        self.random_initial = random_initial
+        self.random_adjust = random_adjust
         self.stepsize = stepsize
         self.diff = diff
-        self.tol = tol
+        self.max_increase_n_step = max_increase_n_step
+        self.energy_list=[]
+        self.n_step=n_step
+        self.lowest_energy=NOT_DEFINED
+        self.lowest_energy_circuit=None
 
-    def run_optimization(self, _circuit, hamiltonian):
+    def run_optimization(self, _circuit:BlockCircuit, hamiltonian):
+        """
+        Carry out ImaginaryTimeEvolution on the active positions of the BlockCircuit provided.
+        """
+        start_energy=get_circuit_energy(_circuit,hamiltonian)
+        self.lowest_energy=start_energy
+        self.lowest_energy_circuit=_circuit
+        self.energy_list.append(start_energy)
 
         circuit=_circuit.duplicate()
-        n_parameter = circuit.get_active_n_parameter()
+        original_parameter=circuit.get_parameter_on_active_position()
 
-        initial_parameter = [0.0]*n_parameter
+        n_parameter = circuit.count_n_parameter_on_active_position()
 
-        if self.random_initial != 0:
-            initial_parameter = random_list(-self.random_initial,
-                                            self.random_initial, n_parameter)
-            for i in range(n_parameter):
-                circuit.adjust_parameter_by_postion(initial_parameter[i], i)
+        if n_parameter==0:
+            #If active position list is empty, make all the position active
+            circuit.active_position_list=list(range(len(circuit.block_list)))
+            n_parameter=circuit.count_n_parameter_on_active_position()
 
-        print(circuit)
-        print(get_circuit_energy(circuit, hamiltonian))
+        if self.random_adjust != 0:
+            random_adjust_list = random_list(-self.random_adjust,
+                                            self.random_adjust, n_parameter)
+            circuit.adjust_parameter_on_active_position(random_adjust_list)
+        
+        
+        previous_energy=self.lowest_energy
+        increase_n_step=0
 
-        for i in range(0, 10):
+        for i in range(0, self.n_step):
 
             mat_A = [[0.0 for col in range(n_parameter)]
                      for row in range(n_parameter)]
             mat_C = [0.0 for col in range(n_parameter)]
 
-            adjusted_circuits = [None for col in range(n_parameter)]
+            adjusted_circuits = []
 
             diff = self.diff
-
-            # adjusted_circuit=circuit.duplicate()
-            # adjusted_circuit.adjust_parameter_by_postion(diff,1)
-            # adjusted_circuits[1]=adjusted_circuit
-
-            for i in range(n_parameter):
-                adjusted_circuit = circuit.duplicate()
-                adjusted_circuit.adjust_parameter_by_postion(diff, i)
-                adjusted_circuits[i] = adjusted_circuit
-                # print(adjusted_circuit)
+            for position in circuit.active_position_list:
+                block_n_para=circuit.block_list[position].n_parameter
+                for in_block_position in range(block_n_para):
+                    adjust_list=[0.0]*block_n_para
+                    adjust_list[in_block_position]+=diff
+                    adjusted_circuit = circuit.duplicate()
+                    adjusted_circuit.adjust_parameter_by_block_postion(adjust_list,position)
+                    adjusted_circuits.append(adjusted_circuit)
 
             for i in range(n_parameter):
                 for j in range(i, n_parameter):
@@ -67,7 +83,7 @@ class ImaginaryTimeEvolutionOptimizer(ParameterOptimizer):
                     mat_A[i][j] = term_value
                     mat_A[j][i] = term_value
 
-            # print(mat_A)
+            #print(mat_A)
 
             origin_energy = get_circuit_energy(circuit, hamiltonian)
             for i in range(n_parameter):
@@ -77,17 +93,36 @@ class ImaginaryTimeEvolutionOptimizer(ParameterOptimizer):
                 term_value *= -0.5
                 mat_C[i] = term_value
 
-            # print(mat_C)
+            #print(mat_C)
+
             try:
                 ITE_derivative = linalg.solve(mat_A, mat_C)
             except linalg.LinAlgError:
-                ITE_derivative = random_list(-self.random_initial,
-                                            self.random_initial, n_parameter)
+                ITE_derivative = numpy.array(random_list(-self.random_adjust,
+                                            self.random_adjust, n_parameter))
 
-            ITE_derivative /= linalg.norm(ITE_derivative)
-            ITE_derivative *= self.stepsize
+            ITE_derivative_norm = linalg.norm(ITE_derivative)
+            ITE_derivative *= self.stepsize/ITE_derivative_norm
+            circuit.adjust_parameter_on_active_position(ITE_derivative)
 
-            # print(ITE_derivative)
-            circuit.adjust_parameter_by_list(ITE_derivative)
-            print(circuit)
-            print(get_circuit_energy(circuit, hamiltonian))
+            energy=get_circuit_energy(circuit, hamiltonian)
+            self.energy_list.append(energy)
+
+            if energy<self.lowest_energy:
+                self.lowest_energy=energy
+                self.lowest_energy_circuit=circuit
+
+            if energy>=previous_energy:
+                increase_n_step+=1
+
+            previous_energy=energy
+            print(self.lowest_energy,self.energy_list)
+
+            if increase_n_step>=self.max_increase_n_step:
+                break
+        optimal_parameter=self.lowest_energy_circuit.get_parameter_on_active_position()
+        amp=[]
+        for i in range(len(original_parameter)):
+            amp.append(optimal_parameter[i]-original_parameter[i])
+        #print(original_parameter,optimal_parameter,amp)
+        return self.lowest_energy,amp
