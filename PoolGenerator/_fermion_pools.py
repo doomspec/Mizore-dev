3c78ad5d40c32895382de8ccdf063a71056e34c5
CopperHu <41934310+CopperHu@users.noreply.github.com>
2020-07-23 16:32:40 +0800

    Update _fermion_pools.py

--- PoolGenerator/_fermion_pools.py
+++ PoolGenerator/_fermion_pools.py
@@ -15,7 +15,7 @@ def fermion_single_double_excitation_pool(n_qubit, hamiltonian=None, fermi_qubit
         MultiRotationEntangler
     """
     if hamiltonian == None:
-        hamiltonian = uccgsd_generator(n_qubit)
+        hamiltonian = general_single_generator(n_qubit)+uccgsd_double_generator(n_qubit)
 
     from Utilities.Iterators import iter_terms_in_fermion_operator
     
@@ -24,14 +24,180 @@ def fermion_single_double_excitation_pool(n_qubit, hamiltonian=None, fermi_qubit
         qubit_excitation_operator=fermi_qubit_transform(hamiltonian)
         yield MultiRotationEntangler(qubit_excitation_operator)
 
-def get_uccgsd_parameter_number(n_qubit):
+
+def upccgsd_pool(n_qubit, fermi_qubit_transform=bravyi_kitaev):
+    """Pools proposed in J. Chem. Theory Comput. 2018, 15, 311–324., also called k-UpCCGSD.
+    Operators are single and pair double unitary excitation operators.
+    Args:
+        n_qubit: number of qubits to generate excitation operators
+        packed_amplitudes: variational parameters, default all 0
+        fermi_qubit_transform: transformation, default bravyi_kitaev
+    Return:
+        Class MultiRotationEntangler with attribute QubitOperator
+    """
+    
+    hamiltonian = general_single_generator(n_qubit)+upccgsd_double_generator(n_qubit)
+    from Utilities.Iterators import iter_terms_in_fermion_operator
+    
+    for term in iter_terms_in_fermion_operator(hamiltonian):
+        print(term)
+        qubit_excitation_operator=fermi_qubit_transform(hamiltonian)
+        yield MultiRotationEntangler(qubit_excitation_operator)
+
+
+def get_general_single_parameter_number(n_qubit):
+    n_spatial_orbitals = n_qubit // 2
+    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
+    n_parameters = 2*n_single_amplitudes
+    return n_parameters
+
+def get_uccgsd_double_parameter_number(n_qubit):
+    n_spatial_orbitals = n_qubit // 2
+    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
+    n_parameters = n_single_amplitudes*n_single_amplitudes
+    return n_parameters
+
+def get_upccgsd_double_parameter_number(n_qubit):
+    n_spatial_orbitals = n_qubit // 2
+    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
+    n_parameters = n_single_amplitudes
+    return n_parameters
+
+""" def get_uccgsd_parameter_number(n_qubit):
     n_spatial_orbitals = n_qubit // 2
     n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
     n_parameters = 2*n_single_amplitudes+n_single_amplitudes*n_single_amplitudes
     return n_parameters
 
+def get_upccgsd_parameter_number(n_qubit):
+    n_spatial_orbitals = n_qubit // 2
+    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
+    n_parameters = 3*n_single_amplitudes
+    return n_parameters
+ """
 
-def uccgsd_generator(n_qubit, anti_hermitian=True):
+def general_single_generator(n_qubit, anti_hermitian=True):
+    if n_qubit % 2 != 0:
+        raise ValueError('The total number of spin-orbitals should be even.')
+
+    n_spatial_orbitals = n_qubit // 2
+    # Unpack amplitudes
+    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
+    n_parameters = get_general_single_parameter_number(n_qubit)
+    packed_amplitudes = [1]*n_parameters
+    # Single amplitudes
+    t1_1 = packed_amplitudes[:n_single_amplitudes]
+    t1_2 = packed_amplitudes[n_single_amplitudes:2*n_single_amplitudes]
+
+    # Initialize operator
+    generator = FermionOperator()
+
+    # Generate excitations
+    count = 0
+    for i in range(n_spatial_orbitals):
+        for a in range(i + 1, n_spatial_orbitals):
+            # Generate single excitations
+            coeff = t1_1[count]
+            generator += FermionOperator((
+                (2*a, 1),
+                (2*i, 0)),
+                coeff)
+            if anti_hermitian:
+                generator += FermionOperator((
+                    (2*i, 1),
+                    (2*a, 0)),
+                    -coeff)
+
+            coeff = t1_2[count]
+            generator += FermionOperator((
+                (2*a+1, 1),
+                (2*i+1, 0)),
+                coeff)
+            if anti_hermitian:
+                generator += FermionOperator((
+                    (2*i+1, 1),
+                    (2*a+1, 0)),
+                    -coeff)
+
+            count += 1
+
+    return generator
+
+def uccgsd_double_generator(n_qubit, anti_hermitian=True):
+    if n_qubit % 2 != 0:
+        raise ValueError('The total number of spin-orbitals should be even.')
+
+    n_spatial_orbitals = n_qubit // 2
+    # Unpack amplitudes
+    n_parameters = get_uccgsd_double_parameter_number(n_qubit)
+    packed_amplitudes = [1]*n_parameters
+
+    # Initialize operator
+    generator = FermionOperator()
+
+    # Generate excitations
+    count_d = 0
+    for i in range(n_spatial_orbitals):
+        for a in range(i + 1, n_spatial_orbitals):
+            # Generate double excitations
+            for j in range(n_spatial_orbitals):
+                for b in range(j + 1, n_spatial_orbitals):
+                    coeff = packed_amplitudes[count_d]
+                    generator += FermionOperator((
+                        (2*a, 1),
+                        (2*i, 0),
+                        (2*b+1, 1),
+                        (2*j+1, 0)),
+                        coeff)
+                    if anti_hermitian:
+                        generator += FermionOperator((
+                            (2*i, 1),
+                            (2*a, 0),
+                            (2*j+1, 1),
+                            (2*b+1, 0)),
+                            -coeff)
+
+                    count_d += 1
+
+    return generator
+
+def upccgsd_double_generator(n_qubit, anti_hermitian=True):
+    if n_qubit % 2 != 0:
+        raise ValueError('The total number of spin-orbitals should be even.')
+
+    n_spatial_orbitals = n_qubit // 2
+    # Unpack amplitudes
+    n_parameters = get_upccgsd_double_parameter_number(n_qubit)
+    packed_amplitudes = [1]*n_parameters
+
+    # Initialize operator
+    generator = FermionOperator()
+
+    # Generate excitations
+    count_d = 0
+    for i in range(n_spatial_orbitals):
+        for a in range(i + 1, n_spatial_orbitals):
+            # Generate double excitations
+            coeff = packed_amplitudes[count_d]
+            generator += FermionOperator((
+                (2*a, 1),
+                (2*i, 0),
+                (2*a+1, 1),
+                (2*i+1, 0)),
+                coeff)
+            if anti_hermitian:
+                generator += FermionOperator((
+                    (2*i, 1),
+                    (2*a, 0),
+                    (2*i+1, 1),
+                    (2*a+1, 0)),
+                    -coeff)
+        
+            count_d += 1
+
+    return generator
+
+""" def uccgsd_generator(n_qubit, anti_hermitian=True):
     if n_qubit % 2 != 0:
         raise ValueError('The total number of spin-orbitals should be even.')
 
@@ -44,7 +210,7 @@ def uccgsd_generator(n_qubit, anti_hermitian=True):
     # Single amplitudes
     t1_1 = packed_amplitudes[:n_single_amplitudes]
     t1_2 = packed_amplitudes[n_single_amplitudes:2*n_single_amplitudes]
-    # Double amplitudes associated with one pair
+    # Double amplitudes
     t2_1 = packed_amplitudes[2*n_single_amplitudes:]
 
     # Initialize operator
@@ -103,38 +269,15 @@ def uccgsd_generator(n_qubit, anti_hermitian=True):
     return generator
 
 
-def upccgsd_pool(n_qubit, packed_amplitudes=None, fermi_qubit_transform=bravyi_kitaev):
-    """Pools proposed in J. Chem. Theory Comput. 2018, 15, 311–324., also called k-UpCCGSD.
-    Operators are single and pair double unitary excitation operators.
-    Args:
-        n_qubit: number of qubits to generate excitation operators
-        packed_amplitudes: variational parameters, default all 0
-        fermi_qubit_transform: transformation, default bravyi_kitaev
-    Return:
-        Class MultiRotationEntangler with attribute QubitOperator
-    """
-    n_parameters = get_upccgsd_parameter_number(n_qubit)
-    if packed_amplitudes==None:
-        packed_amplitudes = [0.0]*n_parameters
-
-    hamiltonian = fermi_qubit_transform(upccgsd_generator(n_qubit, packed_amplitudes))
-    
-    return MultiRotationEntangler(hamiltonian)
-
-def get_upccgsd_parameter_number(n_qubit):
-    n_spatial_orbitals = n_qubit // 2
-    n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
-    n_parameters = 3*n_single_amplitudes
-    return n_parameters
-
-def upccgsd_generator(n_qubit, packed_amplitudes, anti_hermitian=True):
+def upccgsd_generator(n_qubit, anti_hermitian=True):
     if n_qubit % 2 != 0:
         raise ValueError('The total number of spin-orbitals should be even.')
 
     n_spatial_orbitals = n_qubit // 2
     # Unpack amplitudes
     n_single_amplitudes = n_spatial_orbitals * (n_spatial_orbitals-1) // 2
-        
+    n_parameters = get_upccgsd_parameter_number(n_qubit)
+    packed_amplitudes = [1]*n_parameters    
     # Single amplitudes
     t1_1 = packed_amplitudes[:n_single_amplitudes]
     t1_2 = packed_amplitudes[n_single_amplitudes:2*n_single_amplitudes]
@@ -191,3 +334,4 @@ def upccgsd_generator(n_qubit, packed_amplitudes, anti_hermitian=True):
 
     return generator
 
+ """
