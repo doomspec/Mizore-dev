2494252d5965ab416c2ce33fb2e0193d18d5b074
CopperHu <41934310+CopperHu@users.noreply.github.com>
2020-08-26 00:43:13 +0800

    Update _quantum_chips.py
    add ionq_full_connected

--- src/HamiltonianGenerator/chips/_quantum_chips.py
+++ src/HamiltonianGenerator/chips/_quantum_chips.py
@@ -10,3 +10,17 @@ def IBM5QYorktown(weights=None):
         G.add_weighted_edges_from([(0, 1, 1), (0, 2, 1), (1, 2, 1)
             , (2, 3, 1), (2, 4, 1), (3, 4, 1)])
     return G
+
+def ionq_full_connected(weights=None):
+    G = nx.Graph()
+    n = 11
+    G.add_nodes_from(range(n))
+    if weights == None:
+        for i in range(n):
+            for j in range(i+1,n):
+                G.add_edge(i, j, weight = 0.975)
+    else:
+        for i in range(n):
+            for j in range(i+1,n):
+                G.add_edge(i, j, weight = weights[0])
+    return G
