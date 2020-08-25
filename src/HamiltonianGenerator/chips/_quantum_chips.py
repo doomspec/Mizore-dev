6b63bd0188ff6284e9bacdd6ac878152735e1a95
CopperHu <41934310+CopperHu@users.noreply.github.com>
2020-08-26 01:03:17 +0800

    Update _quantum_chips.py
    simplyfy some functions

--- src/HamiltonianGenerator/chips/_quantum_chips.py
+++ src/HamiltonianGenerator/chips/_quantum_chips.py
@@ -1,26 +1,17 @@
 import Networkx as nx
 
-def IBM5QYorktown(weights=None):
+def IBM5QYorktown(weights=[1,1,1,1,1,1]):
     G = nx.Graph()
     G.add_nodes_from(range(5))
-    if len(weights) == 6:
-        G.add_weighted_edges_from([(0, 1, weights[0]), (0, 2, weights[1]), (1, 2, weights[2])
+    G.add_weighted_edges_from([(0, 1, weights[0]), (0, 2, weights[1]), (1, 2, weights[2])
             , (2, 3, weights[3]), (2, 4, weights[4]), (3, 4, weights[5])])
-    else:
-        G.add_weighted_edges_from([(0, 1, 1), (0, 2, 1), (1, 2, 1)
-            , (2, 3, 1), (2, 4, 1), (3, 4, 1)])
     return G
 
-def ionq_full_connected(weights=None):
+def ionq_full_connected(weights=0.975):
     G = nx.Graph()
     n = 11
     G.add_nodes_from(range(n))
-    if weights == None:
-        for i in range(n):
-            for j in range(i+1,n):
-                G.add_edge(i, j, weight = 0.975)
-    else:
-        for i in range(n):
-            for j in range(i+1,n):
-                G.add_edge(i, j, weight = weights[0])
+    for i in range(n):
+        for j in range(i+1,n):
+            G.add_edge(i, j, weight = weights)
     return G
