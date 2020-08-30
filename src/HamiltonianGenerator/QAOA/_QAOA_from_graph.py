b4409c44b0cfe522e3d4017f2eee7c1cfa94ceb5
CopperHu <41934310+CopperHu@users.noreply.github.com>
2020-08-30 11:16:53 +0800

    update qaoa

--- src/HamiltonianGenerator/QAOA/_QAOA_from_graph.py
+++ src/HamiltonianGenerator/QAOA/_QAOA_from_graph.py
@@ -9,17 +9,21 @@ def random_graph(d=3, n=8, seed=None):
         n (integer) – The number of nodes. The value of n×d must be even.
         seed (integer, random_state, or None (default)) – 
             Indicator of random number generation state.
+    return: random networkx regular graph with weights. Will also print the graph.
     '''
     graph = nx.random_regular_graph(d, n, seed)
     graph.edges.data("weight", default=1)
-    """ for u,v,d in graph.edges(data=True):
-        d['weight'] = 1.0 """
+    for u,v,d in graph.edges(data=True):
+        d['weight'] = 1.0
+    pos=nx.kamada_kawai_layout(graph)
+    plt.figure(figsize=(4, 4))
+    nx.draw(graph, pos, with_labels=True, font_weight='bold')
+    plt.show()
     return graph
 
 def get_maxcut_hamiltonian_from_graph(graph):
     hamiltonian = QubitOperator()
     for (i, j, wt) in graph.edges.data('weight'):
-        print(i,j,wt)
         hamiltonian += wt * QubitOperator("Z" + str(i) + " Z" + str(j))
 
     return hamiltonian
@@ -32,17 +36,16 @@ def get_tsp_hamiltonian_from_graph(graph):
         for (i, j, wt) in graph.edges.data('weight'):
             hamiltonian += -wt * QubitOperator("Z" + str(i * nodes + s))
             hamiltonian += -wt * QubitOperator("Z" + str(j * nodes + s + 1))
-            hamiltonian += wt * QubitOperator("Z" + str(i * nodes + s) +
-                                "Z" + str(j * nodes + s + 1))  
+            hamiltonian += wt * QubitOperator("Z" + str(i * nodes + s) + " Z" + str(j * nodes + s + 1))  
     return hamiltonian
 
 def get_random_maxcut_hamiltonian(d=3, n=8, seed=None):
-    g = nx.random_regular_graph(d, n, seed)
+    g = random_graph(d, n, seed)
     hamiltonian = get_maxcut_hamiltonian_from_graph(g)
     return hamiltonian
 
 def get_random_tsp_hamiltonian(d=3, n=8, seed=None):
-    g = nx.random_regular_graph(d, n, seed)
+    g = random_graph(d, n, seed)
     hamiltonian = get_tsp_hamiltonian_from_graph(g)
     return hamiltonian
 
