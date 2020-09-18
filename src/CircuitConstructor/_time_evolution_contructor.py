b0267cb197b8fbb3150c82a573fe31aa81a17d37
doomspec <doomspec@outlook.com>
2020-09-18 18:04:57 +0800

    Benchmark of RTS

--- src/CircuitConstructor/_time_evolution_contructor.py
+++ src/CircuitConstructor/_time_evolution_contructor.py
@@ -3,10 +3,14 @@ from Objective._vqs_quality_obj import CircuitQualityObjective
 from ParameterOptimizer import RealTimeEvolutionOptimizer
 from ParameterOptimizer._rte_analytical_optimizer import RTEAnalyticalOptimizer
 import time,json,pickle,os
+from Blocks import TimeEvolutionBlock,BlockCircuit
+from Blocks._utilities import get_inner_two_circuit_product
+from Blocks._trotter_evolution_block import TrotterTimeEvolutionBlock
 
 class TimeEvolutionConstructor():
 
     def __init__(self, energy_obj, pool, init_circuit, project_name="Untitled",task_manager=None,is_analytical=False, n_block_per_iter=1,quality_cutoff=0.0001, diff=1e-4, n_circuit=3, stepsize=1e-3,delta_t=0.1):
+        
         self.energy_obj = energy_obj
         self.circuit = init_circuit
         self.quality_cutoff = quality_cutoff
@@ -54,7 +58,7 @@ class TimeEvolutionConstructor():
 
             if construct_needed:
                 constructor = NoParameterConstructor(
-                    self.quality_obj, self.pool, task_manager=self.task_manager,terminate_cost=self.quality_cutoff, init_circuit=self.circuit,n_block_per_iter=self.n_block_per_iter,not_save=True)
+                    self.quality_obj, self.pool, task_manager=self.task_manager,terminate_cost=self.quality_cutoff/2, init_circuit=self.circuit,n_block_per_iter=self.n_block_per_iter,not_save=True)
                 new_circuit = constructor.run()
                 if constructor.current_cost>self.quality_cutoff-1e-7:
                     print("Circuit constructor can not reach the object quality, consider using a larger pool!!!")
@@ -107,6 +111,75 @@ class TimeEvolutionConstructor():
         path = self.save_path + "/energy_obj.pickle"
         with open(path, "wb") as f:
             pickle.dump(self.energy_obj, f)
+
+def generate_benchmark(circuit_list,hamiltonian,delta_t):
+
+    n_circuit=len(circuit_list)
+    init_circuit=circuit_list[0]
+    benchmark_circuits=[init_circuit]
+    for step in range(1,n_circuit):
+        bc=init_circuit.duplicate()
+        bc.add_block(TimeEvolutionBlock(hamiltonian,init_angle=delta_t*step))
+        benchmark_circuits.append(bc)
+    fidelity_list=[0]*n_circuit
+    fidelity_list[0]=1
+    for step in range(1,n_circuit):
+        fidelity_list[step]=abs(get_inner_two_circuit_product(circuit_list[step],benchmark_circuits[step]))
+
+    return fidelity_list
+
+def generate_trotter_benchmark_from_file(path,n_trotter_step):
+    with open(path +"/energy_obj.pickle", "rb") as f:
+        hamiltonian=pickle.load(f).hamiltonian
+
+    with open(path + "/run_info.json", "r") as f:
+        log_dict=json.load(f)
+
+    delta_t=log_dict["delta_t"]
+    circuit_path=path+"/0.bc"
+    with open(circuit_path, "rb") as f:
+        init_circuit=pickle.load(f)
+
+    n_circuit=1
+    while True:
+        circuit_path=path+"/"+str(n_circuit)+".bc"
+        if not os.path.exists(circuit_path):
+            break
+        n_circuit+=1
+
+    return generate_trotter_benchmark(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit)
+
+def generate_trotter_benchmark(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit):
+    circuit_list=[init_circuit.duplicate()]
+    for step in range(1,n_circuit):
+        bc=init_circuit.duplicate()
+        bc.add_block(TrotterTimeEvolutionBlock(hamiltonian,n_trotter_step=n_trotter_step*step,evolution_time=delta_t*step))
+        circuit_list.append(bc)
+    return generate_benchmark(circuit_list,hamiltonian,delta_t)
+
+def generate_benchmark_from_file(path):
+
+    with open(path +"/energy_obj.pickle", "rb") as f:
+        hamiltonian=pickle.load(f).hamiltonian
+
+    with open(path + "/run_info.json", "r") as f:
+        log_dict=json.load(f)
+
+    delta_t=log_dict["delta_t"]
+
+    circuit_list=[]
+    circuit_index=0
+    while True:
+        circuit_path=path+"/"+str(circuit_index)+".bc"
+        if not os.path.exists(circuit_path):
+            break
+        with open(circuit_path, "rb") as f:
+            step_circuit=pickle.load(f)
+        circuit_list.append(step_circuit)
+        circuit_index+=1
+    #print(hamiltonian)
+    #print("delta_t",delta_t)
+    return generate_benchmark(circuit_list,hamiltonian,delta_t)    
 
 
 def mkdir(path):
