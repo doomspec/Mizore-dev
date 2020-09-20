b71d757d1fe644ebe3d2af478d935af7c2f8499a
doomspec <doomspec@outlook.com>
2020-09-21 01:06:30 +0800

    rts benchmark generator

--- src/CircuitConstructor/_time_evolution_contructor.py
+++ src/CircuitConstructor/_time_evolution_contructor.py
@@ -70,7 +70,9 @@ class TimeEvolutionConstructor():
                 new_circuit = self.circuit
 
             local_time_to_evolve = self.delta_t-(total_time_evolved % self.delta_t)
-            local_time_to_evolve = np.round(local_time_to_evolve,10)
+            if local_time_to_evolve < 1e-12:
+                local_time_to_evolve=self.delta_t
+
             print("local_time_to_evolve",local_time_to_evolve)
 
             new_circuit,evolved_time = self.evolver.do_time_evolution(
@@ -86,7 +88,7 @@ class TimeEvolutionConstructor():
 
             construct_needed = (abs(evolved_time-local_time_to_evolve)>=1e-7)
 
-            if (not construct_needed) and (total_time_evolved>last_evolved_time):
+            if ((not construct_needed) and (total_time_evolved>last_evolved_time)):
                 circuit_list.append(new_circuit.duplicate())
                 last_evolved_time=total_time_evolved
                 new_circuit.save_self_file(self.save_path,str(len(circuit_list)-1))
@@ -127,21 +129,25 @@ class TimeEvolutionConstructor():
         with open(path, "w") as f:
             json.dump(log_dict, f)
 
-def generate_benchmark(circuit_list,hamiltonian,delta_t):
-
-    n_circuit=len(circuit_list)
-    init_circuit=circuit_list[0]
+def generate_benchmark_for_compare(circuit_list_list,hamiltonian,delta_t):
+    list_n_circuit=[len(circuit_list) for circuit_list in circuit_list_list]
+    init_circuit=circuit_list_list[0][0]
     benchmark_circuits=[init_circuit]
-    for step in range(1,n_circuit):
+    for step in range(1,max(list_n_circuit)):
         bc=init_circuit.duplicate()
-        bc.add_block(TimeEvolutionBlock(hamiltonian,init_angle=-delta_t*step))
+        bc.add_block(TimeEvolutionBlock(hamiltonian,init_angle=delta_t*step))
         benchmark_circuits.append(bc)
-    fidelity_list=[0]*n_circuit
-    fidelity_list[0]=1
-    for step in range(1,n_circuit):
-        fidelity_list[step]=abs(get_inner_two_circuit_product(circuit_list[step],benchmark_circuits[step]))
+    fidelity_list_list=[]
+    for i in range(len(circuit_list_list)):
+        fidelity_list=[0]*list_n_circuit[i]
+        fidelity_list[0]=1
+        for step in range(1,list_n_circuit[i]):
+            fidelity_list[step]=abs(get_inner_two_circuit_product(circuit_list_list[i][step],benchmark_circuits[step]))
+        fidelity_list_list.append(fidelity_list)
+    return fidelity_list_list
 
-    return fidelity_list
+def generate_benchmark(circuit_list,hamiltonian,delta_t):
+    return generate_benchmark_for_compare([circuit_list],hamiltonian,delta_t)[0]
 
 def generate_trotter_benchmark_from_file(path,n_trotter_step):
     with open(path +"/energy_obj.pickle", "rb") as f:
@@ -164,12 +170,16 @@ def generate_trotter_benchmark_from_file(path,n_trotter_step):
 
     return generate_trotter_benchmark(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit)
 
-def generate_trotter_benchmark(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit):
+def generate_trotter_benchmark_circuits(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit):
     circuit_list=[init_circuit.duplicate()]
     for step in range(1,n_circuit):
         bc=init_circuit.duplicate()
         bc.add_block(TrotterTimeEvolutionBlock(hamiltonian,n_trotter_step=n_trotter_step*step,evolution_time=delta_t*step))
         circuit_list.append(bc)
+    return circuit_list
+
+def generate_trotter_benchmark(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit):
+    circuit_list=generate_trotter_benchmark_circuits(init_circuit,n_trotter_step,hamiltonian,delta_t,n_circuit)
     return generate_benchmark(circuit_list,hamiltonian,delta_t)
 
 def generate_benchmark_from_file(path):
@@ -182,6 +192,11 @@ def generate_benchmark_from_file(path):
 
     delta_t=log_dict["delta_t"]
 
+    circuit_list=read_circuits_from_file(path)
+    
+    return generate_benchmark(circuit_list,hamiltonian,delta_t)  
+
+def read_circuits_from_file(path):
     circuit_list=[]
     circuit_index=0
     while True:
@@ -192,9 +207,120 @@ def generate_benchmark_from_file(path):
             step_circuit=pickle.load(f)
         circuit_list.append(step_circuit)
         circuit_index+=1
-    #print(hamiltonian)
-    #print("delta_t",delta_t)
-    return generate_benchmark(circuit_list,hamiltonian,delta_t)    
+    return circuit_list
+
+def save_run_status_info(self):
+        log_dict = {}
+        log_dict["n_block_change"]=self.n_block_change
+        log_dict["quality_list"]=self.quality_list.tolist()
+        log_dict["evolution_time_list"]=self.evolution_time_list.tolist()
+        path = self.save_path + "/run_status_info.json"
+        with open(path, "w") as f:
+            json.dump(log_dict, f)
+
+def draw_run_status_figure(path):
+
+    with open(path +"/energy_obj.pickle", "rb") as f:
+        hamiltonian=pickle.load(f).hamiltonian
+    with open(path+ "/run_status_info.json", "r") as f:
+        log_dict=json.load(f)
+    n_block_change=log_dict["n_block_change"]
+    gate_name="CNOT"
+    n_gate_list=[]
+    n_gate_time_list=[]
+    for item in n_block_change:
+        n_gate_time_list.append(item[0])
+        n_gate_list.append(item[2][gate_name])
+    quality_list=log_dict["quality_list"]
+    evolution_time_list=log_dict["evolution_time_list"]
+    first_trotter_gate_use=TrotterTimeEvolutionBlock(hamiltonian,n_trotter_step=1,evolution_time=0.1).get_gate_used()[gate_name]
+    end_time=evolution_time_list[-1]
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    gate_name="CNOT"
+    n_gate_list=[]
+    n_gate_time_list=[]
+    n_block_change.append((end_time,None,None))
+    for i in range(len(n_block_change)-1):
+        n_gate_time_list.append(n_block_change[i][0])
+        n_gate_list.append(n_block_change[i][2][gate_name])
+        n_gate_time_list.append(n_block_change[i+1][0])
+        n_gate_list.append(n_block_change[i][2][gate_name])
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    lns1=ax.plot(evolution_time_list, quality_list, '-',color="dodgerblue",label="Distance")
+
+    ax2 = ax.twinx()
+    lns2=ax2.plot(n_gate_time_list, n_gate_list, '-',color="orange",label=gate_name+" count: Adaptive")
+    lns3=ax2.plot([0,end_time], [first_trotter_gate_use,first_trotter_gate_use], '-',color="red",label=gate_name+" count: 1st Trotter ")
+    ax.grid()
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Distance")
+    ax2.set_ylabel("Number of gates")
+
+    lns = lns2+lns1+lns3
+    labs = [l.get_label() for l in lns]
+    ax.legend(lns, labs, loc='upper right',ncol=2)
+
+    ax2.set_ylim(0, first_trotter_gate_use*1.2)
+    ax.set_ylim(0,max(quality_list)*1.4)
+    plt.savefig(path+'/run_status.png',bbox_inches='tight')
+
+def generate_benchmark_for_compare_from_paths(paths):
+    label_list=[]
+    delta_t_list=[]
+    circuit_list_list=[]
+    for path in paths:
+        circuit_list=read_circuits_from_file(path)
+        circuit_list_list.append(circuit_list)
+        with open(path + "/run_info.json", "r") as f:
+            log_dict=json.load(f)
+        label_list.append("D_cut "+str(log_dict["quality_cutoff"]))
+        delta_t_list.append(log_dict["delta_t"])
+    delta_t=max(delta_t_list)
+    assert delta_t==min(delta_t_list)
+    init_circuit=circuit_list_list[0][0]
+    with open(paths[0] +"/energy_obj.pickle", "rb") as f:
+        hamiltonian=pickle.load(f).hamiltonian
+
+    for n_trotter_step in [1,2]:
+        list_n_circuit=[len(circuit_list) for circuit_list in circuit_list_list]
+        max_n_circuit=max(list_n_circuit)
+        trotter_circuits=generate_trotter_benchmark_circuits(init_circuit,n_trotter_step,hamiltonian,delta_t,max_n_circuit)
+        circuit_list_list.append(trotter_circuits)
+        label_list.append("Trotter "+str(n_trotter_step))
+    fidelity_list_list=generate_benchmark_for_compare(circuit_list_list,hamiltonian,delta_t)
+    return fidelity_list_list,label_list,delta_t
+
+def draw_benchmark_for_compare_from_paths(paths):
+    fidelity_list_list,label_list,delta_t=generate_benchmark_for_compare_from_paths(paths)
+    min_fidelity=1
+
+    import matplotlib
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot()
+
+    for i in range(len(fidelity_list_list)):
+        fidelity_list=np.array(fidelity_list_list[i])
+        x_data=[step*delta_t for step in range(len(fidelity_list))]
+        ax.plot(x_data, (-1*fidelity_list)+1, '-o',label=label_list[i])
+        local_min_fidelity=min(fidelity_list)
+        if local_min_fidelity<min_fidelity:
+            min_fidelity=local_min_fidelity
+
+    ax.grid()
+    ax.set_xlabel("Time")
+    ax.set_ylabel("1-Fidelity")
+    ax.legend(loc='upper right',ncol=2)
+    max_diff=1-min_fidelity
+    ax.set_ylim(-max_diff*0.2,max_diff*1.2)
+
+    for path in paths:
+        plt.savefig(path+'/comprison.png',bbox_inches='tight')
 
 
 def mkdir(path):
