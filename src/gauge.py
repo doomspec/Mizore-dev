78a40037925ecd026206bdda1a3526c3192d1b2b
louiisliu <11610522@mail.sustech.edu.cn>
2020-09-26 09:53:00 +0800

    Add gate efficiency

--- src/gauge.py
+++ src/gauge.py
@@ -3,14 +3,25 @@ import numpy as np
 from Utilities.CircuitEvaluation import get_quantum_engine
 from projectq.ops import All, Measure
 from Blocks import BlockCircuit
-from Blocks import HardwareEfficientEntangler, RotationEntangler, HartreeFockInitBlock, SingleParameterMultiRotationEntangler
+from Blocks import HardwareEfficientEntangler, RotationEntangler, HartreeFockInitBlock, \
+    SingleParameterMultiRotationEntangler
 from ParameterOptimizer import BasinhoppingOptimizer
 from Objective import EnergyObjective, AmplitudeObjective
 from openfermion.ops import QubitOperator
 from Benchmark.MaxCut.util import dec2bin
 from HamiltonianGenerator import make_example_H2
 
-def get_coverage(bc: BlockCircuit, state_init = '0000', state_j = '1111'): 
+
+def get_gate_efficiency(bc: BlockCircuit, n: int):
+    obj = AmplitudeObjective(n)
+    optimizer = BasinhoppingOptimizer(random_initial=0.1)
+    circuit = BlockCircuit(n)
+    circuit.add_block(bc)
+    return optimizer.run_optimization(circuit, obj.get_cost()), optimizer.run_optimization(circuit,
+                                                                                           obj.get_cost(maximunm=True))
+
+
+def get_coverage(bc: BlockCircuit, state_init='0000', state_j='1111'):
     n_qubit = len(state_init)
     init_set = []
     for i, value in enumerate(state_init):
@@ -21,30 +32,32 @@ def get_coverage(bc: BlockCircuit, state_init = '0000', state_j = '1111'):
     for i, value in enumerate(state_j):
         if value == '1':
             j_set.append(i)
-            
+
     coverage = []
     init_block = HartreeFockInitBlock(init_set)
     final_block = HartreeFockInitBlock(j_set)
     obj = AmplitudeObjective(n_qubit)
     for i, block in enumerate(bc.block_list):
         circuit = BlockCircuit(n_qubit)
-        circuit.add_block(init_block) 
-        for j in range(i+1):
+        circuit.add_block(init_block)
+        for j in range(i + 1):
             circuit.add_block(bc.block_list[j])
         circuit.add_block(final_block)
-        optimizer = BasinhoppingOptimizer(random_initial=0.1) # The initial parameter will be a random value between -0.1 and +0.1
-        
+        optimizer = BasinhoppingOptimizer(
+            random_initial=0.1)  # The initial parameter will be a random value between -0.1 and +0.1
+
         coverage_low, para = optimizer.run_optimization(circuit, obj.get_cost())
         coverage_high, para = optimizer.run_optimization(circuit, obj.get_cost(maximum=True))
         coverage.append(- coverage_high - coverage_low)
 
     return coverage
 
-def get_parameter_efficiency(bc: BlockCircuit, state_init = '0000'): 
+
+def get_parameter_efficiency(bc: BlockCircuit, state_init='0000'):
     n_qubit = len(state_init)
     efficiency = [0.0 for i in range(len(bc.block_list))]
     n_parameter = []
-    
+
     for i, block in enumerate(bc.block_list):
         n_parameter.append(block.n_parameter)
 
@@ -55,24 +68,26 @@ def get_parameter_efficiency(bc: BlockCircuit, state_init = '0000'):
             state_j += str(value)
         """ print(state_j)
         break """
-        coverage = get_coverage(bc, state_init, state_j) #list of coverage
+        coverage = get_coverage(bc, state_init, state_j)  # list of coverage
         for i, block in enumerate(bc.block_list):
             efficiency[i] += coverage[i] / n_parameter[i]
 
     return efficiency
 
-def output_region(bc: BlockCircuit, obj, state_init = '0000'):  
+
+def output_region(bc: BlockCircuit, obj, state_init='0000'):
     n_qubit = len(state_init)
     efficiency = [0.0 for i in range(len(bc.block_list))]
 
     for i, block in enumerate(bc.block_list):
         circuit = BlockCircuit(n_qubit)
-        circuit.add_block(init_block) 
-        for j in range(i+1):
+        circuit.add_block(init_block)
+        for j in range(i + 1):
             circuit.add_block(bc.block_list[j])
 
-        optimizer = BasinhoppingOptimizer(random_initial=0.1) # The initial parameter will be a random value between -0.1 and +0.1
-        
+        optimizer = BasinhoppingOptimizer(
+            random_initial=0.1)  # The initial parameter will be a random value between -0.1 and +0.1
+
         coverage_low, para = optimizer.run_optimization(circuit, obj.get_cost())
         coverage_high, para = optimizer.run_optimization(circuit, obj.get_cost(maximum=True))
         region.append(- coverage_high - coverage_low)
@@ -80,19 +95,19 @@ def output_region(bc: BlockCircuit, obj, state_init = '0000'):
     return region
 
 
-
 state_init = '0000'
 state_j = '1100'
 n_qubit = len(state_init)
-bc=BlockCircuit(n_qubit)
-bc.add_block(SingleParameterMultiRotationEntangler(0.3*QubitOperator("X" + str(0) + " Y" + str(1))
-            +0.5*QubitOperator("X" + str(1) + " Y" + str(2)), init_angle = [0.5]))
-bc.add_block(RotationEntangler((1,2,3),(3,2,1)))
+bc = BlockCircuit(n_qubit)
+bc.add_block(SingleParameterMultiRotationEntangler(0.3 * QubitOperator("X" + str(0) + " Y" + str(1))
+                                                   + 0.5 * QubitOperator("X" + str(1) + " Y" + str(2)),
+                                                   init_angle=[0.5]))
+bc.add_block(RotationEntangler((1, 2, 3), (3, 2, 1)))
 print(bc)
 
 print(get_coverage(bc, state_init, state_j))
 
-#print(get_parameter_efficiency(bc, state_init))
+# print(get_parameter_efficiency(bc, state_init))
 
-#energy_obj = make_example_H2()
-#print(output_region(bc,energy_obj,state_init))
+# energy_obj = make_example_H2()
+# print(output_region(bc,energy_obj,state_init))
