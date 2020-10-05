e3359a330e680d34264bb654e24c926a62f21a21
CopperHu <41934310+CopperHu@users.noreply.github.com>
2020-10-06 00:34:09 +0800

    update one and two qubit states coverage

--- src/Objective/__init__.py
+++ src/Objective/__init__.py
@@ -1,3 +1,3 @@
 from ._energy_obj import EnergyObjective
 from ._purity_obj import PurityObjective
-from ._amplitude_obj import AmplitudeObjective
+from ._amplitude_obj import AmplitudeObjective, OneDMObjective, TwoDMObjective
--- src/Objective/_amplitude_obj.py
+++ src/Objective/_amplitude_obj.py
@@ -6,9 +6,7 @@ from Blocks import HartreeFockInitBlock
 class AmplitudeObjective(Objective):
     """
     Attributes:
-        init_block: the block usually used for initiate the wave fucntion for the Hamiltonian.
-        A block that produce a state near the ground state should be adopted.
-        Usually, for the molecule, the Hartree Fock initialization is used
+        final_block: the final state to be measure
         obj_info: The dict for additional information of the Hamiltonian, e.g. the HF energy and the ground state energy
     Methods:
         get_cost(): generate a Cost object which can be used by optimizers
@@ -22,12 +20,12 @@ class AmplitudeObjective(Objective):
         return
 
     def get_cost(self, maximum = False):
-        return Cost(maximum)
+        return AmplitudeCost(maximum)
 
 
 from Utilities.CircuitEvaluation import evaluate_ansatz_0000_amplitudes
 
-class Cost(CostFunction):
+class AmplitudeCost(CostFunction):
     def __init__(self, maximum=False):
         self.maximum = maximum
 
@@ -52,4 +50,125 @@ class Cost(CostFunction):
             coverage = -amp*np.conjugate(amp)
         else: 
             coverage = amp*np.conjugate(amp)
+        return coverage
+
+
+class OneDMObjective(Objective):
+    """
+    Attributes:
+        n_qubit: number of qubits.
+        qubit: qubit index.
+        state: state in 1rdm.
+        obj_info: The dict for additional information of the Hamiltonian, e.g. the HF energy and the ground state energy
+    Methods:
+        get_cost(): generate a Cost object which can be used by optimizers
+    """
+
+    def __init__(self, n_qubit, qubit=0, state=0, obj_info={}):
+        self.n_qubit = n_qubit
+        self.obj_info = obj_info
+        self.qubit = qubit
+        self.state = state
+        return
+
+    def get_cost(self, maximum = False):
+        return OneDMCost(maximum, self.qubit, self.state)
+
+
+from Utilities.CircuitEvaluation import evaluate_ansatz_1DMs
+from Utilities.WaveLocalProperties import get_processed_eigv_for_DM
+
+class OneDMCost(CostFunction):
+    def __init__(self, maximum=False, qubit=0, state=0):
+        self.maximum = maximum
+        self.qubit = qubit
+        self.state = state
+
+    def get_cost_obj(self, circuit):
+        pcircuit = circuit.get_ansatz_on_active_position()
+        qubit = self.qubit
+        state = self.state
+        
+        def obj(parameter):
+            OneDM = evaluate_ansatz_1DMs(parameter, pcircuit.n_qubit, pcircuit.ansatz)
+            #print(OneDM)
+            probability = get_processed_eigv_for_DM(OneDM[qubit])[state]
+            if self.maximum :
+                coverage = -probability
+            else: 
+                coverage = probability
+
+            return coverage
+
+        return obj
+
+    def get_cost_value(self, circuit):
+        pcircuit = circuit.get_fixed_parameter_ansatz()
+        qubit = self.qubit
+        state = self.state
+        OneDM = evaluate_ansatz_1DMs([], pcircuit.n_qubit, pcircuit.ansatz)
+        amp = get_processed_eigv_for_DM(OneDM[qubit])[state]
+        if self.maximum :
+            coverage = -amp
+        else: 
+            coverage = amp
+        return coverage
+
+
+class TwoDMObjective(Objective):
+    """
+    Attributes:
+        n_qubit: number of qubits.
+        qubit: qubit pair index.
+        state: state in 1rdm.
+        obj_info: The dict for additional information of the Hamiltonian, e.g. the HF energy and the ground state energy
+    Methods:
+        get_cost(): generate a Cost object which can be used by optimizers
+    """
+
+    def __init__(self, n_qubit, qubit_i=1, qubit_j=0, state=0, obj_info={}):
+        self.n_qubit = n_qubit
+        self.obj_info = obj_info
+        self.qubit_i = qubit_i
+        self.qubit_j = qubit_j
+        self.state = state
+        return
+
+    def get_cost(self, maximum = False):
+        return TwoDMCost(maximum, self.qubit_i, self.qubit_j, self.state)
+
+
+from Utilities.CircuitEvaluation import evaluate_ansatz_2DMs
+from Utilities.WaveLocalProperties import get_processed_eigv_for_DM
+
+class TwoDMCost(CostFunction):
+    def __init__(self, maximum=False, qubit_i=1, qubit_j=0, state=0):
+        self.maximum = maximum
+        self.qubit_i = qubit_i
+        self.qubit_j = qubit_j
+        self.state = state
+
+    def get_cost_obj(self, circuit):
+        pcircuit = circuit.get_ansatz_on_active_position()
+        
+        def obj(parameter):
+            TwoDM = evaluate_ansatz_2DMs(parameter, pcircuit.n_qubit, pcircuit.ansatz)
+            probability = get_processed_eigv_for_DM(TwoDM[self.qubit_i][self.qubit_j])[self.state]
+            if self.maximum :
+                coverage = -probability
+            else: 
+                coverage = probability
+
+            return coverage
+
+        return obj
+
+    def get_cost_value(self, circuit):
+        pcircuit = circuit.get_fixed_parameter_ansatz()
+        TwoDM = evaluate_ansatz_2DMs([], pcircuit.n_qubit, pcircuit.ansatz)
+        amp = get_processed_eigv_for_DM(TwoDM[self.qubit_i][self.qubit_j])[self.state]
+        if self.maximum :
+            coverage = -amp
+        else: 
+            coverage = amp
         return coverage
