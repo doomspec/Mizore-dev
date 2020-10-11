3aa6450b2387af38822402fa37ab40d2400013f3
doomspec <doomspec@outlook.com>
2020-10-12 03:35:32 +0800

    clear files

--- install.sh
+++ install.sh
@@ -1,21 +1,4 @@
-GPU_SIM_PATH="src/GPUSimulator/"
 BASE_DIR=$(pwd)
-
-echo "PWD :"$(pwd)
-
-# Initial submodules
-git submodule init
-git submodule update
-
-# install GPU Simulator
-cd $GPU_SIM_PATH
-echo "GPU Simulator Path: "$(pwd)
-
-# Build GPU Simulator
-mkdir build
-cd build
-cmake ../
-make
 
 # install mizore
 cd $BASE_DIR
